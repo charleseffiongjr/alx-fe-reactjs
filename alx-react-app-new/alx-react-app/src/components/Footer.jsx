ae9ae93aab6b73b9e21d5c35b0e18f5e2e1f8f1f
Charles Effiong <charleseffiong28@gmail.com>
2025-03-10 14:00:12 +0100

    alx-react-app

---
+++ alx-react-app-new/alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+    return (
+        <footer style={{color: 'brown', fontSize: '20px', fontFamily: "sans-serif"}}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent (){
+    return (
+        <main style={{color: 'brown', fontSize: '20px', fontFamily: "sans-serif"}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+export default MainContent;
