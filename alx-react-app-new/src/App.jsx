5f0852ea7010fa34706227cfc2ae085ea50d0b3a
Charles Effiong <charleseffiong28@gmail.com>
2025-03-10 16:30:53 +0100

    alx-react-app-new

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,12 +2,13 @@
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/header';
 import MainContent from './components/MainContent'
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './components/counter';
 
 function App() {
   const [count, setCount] = useState(0)
