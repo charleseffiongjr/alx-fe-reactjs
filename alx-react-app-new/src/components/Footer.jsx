682066630db91d19bc690c1e98ecbe2a35de3fc9
Charles Effiong <charleseffiong28@gmail.com>
2025-03-10 14:59:08 +0100

    alx-react-app-new

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer () {
     return (
-        <footer>
+        <footer  style={{ color: 'brown', fontFamily: "sans-serif", fontSize: "20px"}}>
             <p>© 2023 City Lovers</p>
         </footer>
     );
