19b5a7666411ec69e5d5fca9c80202043ccdc7dd
Charles Effiong <charleseffiong28@gmail.com>
2025-03-24 05:13:58 +0100

    recipe-sharing-app

--- recipe-sharing-app/src/components/SearchBar.jsx
+++ recipe-sharing-app/src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import  useRecipeStore  from '.store/useRecipeStore';
+import  useRecipeStore  from '.components/recipeStore';
 
 const SearchBar = () => {
   const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,18 +2,21 @@ import { create } from "zustand";
 
 const recipeStore = create((set) => ({
   recipes: [],
+  searchTerm: "",
+  setSearchTerm: (term) => set(() => ({ searchTerm: term })),
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-    setRecipes: (recipes) => set({ recipes }),
-  updateRecipe: (updatedRecipe) => set((state) => ({
-    recipes: state.recipes.map((recipe) =>
-      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-    ),
-  })),
-  deleteRecipe: (recipeId) => set((state) => ({
-    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
-  })),
-
+  setRecipes: (recipes) => set({ recipes }),
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+  deleteRecipe: (recipeId) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+    })),
 }));
 
 export default recipeStore;
