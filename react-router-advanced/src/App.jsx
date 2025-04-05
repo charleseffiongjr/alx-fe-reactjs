a08ac07e1dea0a4b9e5573aea1437f17f6cb4591
Charles Effiong <charleseffiong28@gmail.com>
2025-04-05 10:12:19 +0100

    react-router-advanced

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, BrowserRouter } from 'react-router-dom';
 import Profile from "./components/Profile";
 import ProfileDetails from "./components/ProfileDetails";
 import ProfileSettings from "./component/ProfileSettings";
 import UserProfile from "./component/UserProfile";
 import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./components/Home";
+
 
 function App() {
 
-
+const [count, setCount] = useState(0)
   return (
     <>
+    <BrowserRouter>
       <Router>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -40,7 +43,8 @@ function App() {
 
         <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
-    </Router>
+        </Router>
+        </BrowserRouter>
     </> 
   )
 }
--- react-router-advanced/src/context/AuthContext.jsx
+++ react-router-advanced/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-// src/context/AuthContext.jsx
+
 import { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext();
