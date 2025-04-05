aa43988216d9967934e72a12c0425d8a8c7d2679
Charles Effiong <charleseffiong28@gmail.com>
2025-04-05 08:17:37 +0100

    react-router-advanced

---
+++ react-router-advanced/src/components/Home.jsx
@@ -0,0 +1,27 @@
+import { useAuth } from "../components/AuthContext";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const { isAuthenticated, login, logout } = useAuth();
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Status: {isAuthenticated ? "✅ Logged In" : "❌ Logged Out"}</p>
+      {isAuthenticated ? (
+        <button onClick={logout}>Logout</button>
+      ) : (
+        <button onClick={login}>Login</button>
+      )}
+      <nav>
+        <ul>
+          <li><Link to="/Profile">Profile</Link></li>
+          <li><Link to="/ProfileDetails">Profile Details</Link></li>
+          <li><Link to="/UserProfile">Blog Post 123</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Home;
