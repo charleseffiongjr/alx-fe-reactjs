0b272947d0e509ece368bd5a1e177fd299922fd5
Charles Effiong <charleseffiong28@gmail.com>
2025-04-11 04:43:16 +0100

    recipe-sharing-platform

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -18,8 +18,28 @@ const HomePage = () => {
   }, [])
   
   return (
-    <div className='text-blue-500'>Home Page</div>
-  )
-}
+    <div className='text-blue-500'>
+      <h1>Home Page</h1>
+      {recipes.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className='space-y-4'>
+          {recipes.map((recipe) => (
+            <li key={recipe.id} className='bg-white p-4 rounded shadow'>
+              <h2 className='"text-xl font-semibold'>{recipe.name}</h2>
+              <p> <strong>Ingredients</strong>{recipe.ingredients.join(',')}</p>
+              <p> <strong>Instructions</strong>{recipe.instruction}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+    
+    
+    
+  
 
 export default HomePage
