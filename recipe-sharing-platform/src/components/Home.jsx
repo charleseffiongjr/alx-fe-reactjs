6e6c8eb27e6156df57bc9b67a98ca667cbd6fa4b
Charles Effiong <charleseffiong28@gmail.com>
2025-04-11 03:54:09 +0100

    recipe-sharing-platform

--- recipe-sharing-platform/src/components/Home.jsx
+++ recipe-sharing-platform/src/components/Home.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
-function Home() {
+const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('data.json')
+        const data = await response.json()
+        setRecipes(data);
+      } catch (error) {
+        console.error('Error oading recipe data:', error);
+      }
+      
+    };
+    fetchData();
+  }, [])
+  
   return (
     <div className='text-blue-500'>Home</div>
   )
